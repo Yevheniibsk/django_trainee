b67e5374ec68bacd9fabf167cdfc8b64bfdfbcd7
Hurry Burton <buzdiganoff@gmail.com>
2023-04-02 11:51:19 +0300

    trainee django new files

---
+++ learning_log/learning_logs/urls.py
@@ -0,0 +1,16 @@
+""" Defines URL patterns for learning_logs."""
+
+from django.urls import path
+
+from .import views
+
+app_name = 'learning_logs'
+urlpatterns = [
+    # головна сторінка    
+    path('', views.index, name='index'),
+    # сторінка з темами
+    path('topics/', views.topics, name='topics'),
+    # сторінка присвячена окремій темі.
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+
+]
